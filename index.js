eaef4e42a0b9bcaf65e6170daee84169165cf833
MohammadFaysal731 <www.faysal731@gmail.com>
2022-05-06 16:39:14 +0600

    create Features products api

--- index.js
+++ index.js
@@ -12,13 +12,27 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tcjah.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('connect to db')
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+
+    try {
+        await client.connect();
+        const featuresProductsCollections = client.db('Features').collection('products');
+        // featuresProducts api 
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = featuresProductsCollections.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('storage-devices-server')
